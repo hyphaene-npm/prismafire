f19bd5d8de4f74b52ccabb7cb278d3970b058fad
Maximilien Garenne <maximilien.garenne@gmail.com>
2020-01-03 15:03:00 +0000

    fix(const): reactivate proper glob const

--- utils/constants.js
+++ utils/constants.js
@@ -1,11 +1,9 @@
-const DEFAULT_GLOB = "./datamodels/**/*.graphql";
-// const DEFAULT_GLOB = "./src/datamodels/**/*.graphql";
-
-const DEFAULT_PRISMA_INPUT = "./prisma.json";
-const DEFAULT_PRISMA_OUTPUT = "./prisma.yml";
+const DEFAULT_GLOB = './src/datamodels/**/*.graphql';
+const DEFAULT_PRISMA_INPUT = './prisma.json';
+const DEFAULT_PRISMA_OUTPUT = './prisma.yml';
 
 module.exports = {
 	DEFAULT_GLOB,
 	DEFAULT_PRISMA_INPUT,
-	DEFAULT_PRISMA_OUTPUT,
+	DEFAULT_PRISMA_OUTPUT
 };
