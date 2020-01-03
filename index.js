c69b7f682d19bc1340371139242ab2169e70e43c
Maximilien Garenne <maximilien.garenne@gmail.com>
2020-01-03 15:01:30 +0000

    fix(log): add debugg logs

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 #!/usr/bin/env node
-const fs = require("fs");
-const yaml = require("js-yaml");
-const glob = require("glob");
+const fs = require('fs');
+const yaml = require('js-yaml');
+const glob = require('glob');
 
-const { getPrismaInputPath, getPrismaOutputPath, getGlob } = require("./utils");
+const { getPrismaInputPath, getPrismaOutputPath, getGlob } = require('./utils');
 
 async function main() {
 	return new Promise((resolve, reject) => {
 		glob(getGlob(), function(err, files) {
 			const data = require(getPrismaInputPath());
-
+			console.log({ data });
 			if (!err) {
 				data.datamodel = files;
+				console.log({ files });
 				const yamlStr = yaml.safeDump(data);
-				fs.writeFileSync(getPrismaOutputPath(), yamlStr, "utf8");
-				resolve("prisma file generated !");
+				fs.writeFileSync(getPrismaOutputPath(), yamlStr, 'utf8');
+				resolve('prisma file generated !');
 			} else {
 				resolve(err);
 			}
